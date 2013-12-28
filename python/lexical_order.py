dfb0a18e42174c5be4c48b49d001b6e0196f2e2c
prmkbr <premkabir@gmail.com>
2013-12-28 18:50:41 +0530

    max -> max_num

--- python/lexical_order.py
+++ python/lexical_order.py
@@ -8,18 +8,18 @@ Prints the first 'n' numbers in lexicographical order.
 
 import sys
 
-def print_numbers(start, max):
+def print_numbers(start, max_num):
     i = start
     end = ((start + 10) / 10) * 10 - 1 # Next multiple of 10 minus 1
-    end = min(end, max)
+    end = min(end, max_num)
     while i <= end:
         print i
-        print_numbers(i * 10, max)
+        print_numbers(i * 10, max_num)
         i += 1
 
 def main():
-    max = int(sys.argv[1]) if len(sys.argv) is 2 else 25
-    print_numbers(1, max);
+    max_num = int(sys.argv[1]) if len(sys.argv) is 2 else 25
+    print_numbers(1, max_num);
 
 if __name__ == "__main__":
     main()
