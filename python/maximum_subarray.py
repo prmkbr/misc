fce4dd4587e1def143e19b267b50bd3d05998b1a
prmkbr <premkabir@gmail.com>
2014-02-03 12:47:46 +0530

    Compute the subarray start & end too

--- python/maximum_subarray.py
+++ python/maximum_subarray.py
@@ -12,15 +12,29 @@ import numpy as np
 
 def maximum_subarray(arr):
     overall_max = cur_max = 0
-    for n in arr:
-        cur_max = max(0, cur_max + n)
-        overall_max = max(overall_max, cur_max)
-    return overall_max
+    max_start = max_end = cur_start = cur_end = None
+    for i in xrange(len(arr)):
+        n = arr[i]
+        if (cur_max + n > 0):
+            cur_max += n
+            cur_start = i if cur_start is None else cur_start
+            cur_end = i
+        else:
+            cur_max = 0
+            cur_start = cur_end = None
+
+        if (cur_max > overall_max):
+            overall_max = cur_max
+            max_start = cur_start
+            max_end = cur_end
+
+    return (max_start, max_end, overall_max)
 
 def main():
     na = np.random.randint(low=-20, high=20, size=10)
     print "Array:", na
-    print "Maximum subarray sum:", maximum_subarray(na)
+    res = maximum_subarray(na)
+    print "Maximum subarray: ", res[:2], " sum: ", res[-1]
 
 if __name__ == "__main__":
     main()
